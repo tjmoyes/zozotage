5c450b1542a8b974da3fe171688b704f2efb009f
Tyson Moyes <moye0025@algonquinlive.com>
2021-07-12 22:44:14 -0400

    Oops typo in the shell script

--- zz_A22.sh
+++ zz_A22.sh
@@ -10,10 +10,10 @@ COMPILER=./zz_compiler
 ##
 
 # - Basic Tests (A22 - S21) - - - - - - - - - - - - - - - - - - - - - -
-$COMPILER 1 input/a22_empty.zz  > output/a22_empty.out 2> output/a22_empty.err
+$COMPILER 1 input/a21_empty.zz  > output/a21_empty.out 2> output/a21_empty.err
 $COMPILER 1 input/a22_Numbers.zz  > output/a22_Numbers.out 2> output/a22_Numbers.err
-$COMPILER 1 input/a22_String.zz  > output/a22_String.out 2> output/a22_String.err
-$COMPILER 1 input/a22_General.zz > output/a22_General.out 2> output/a22_General.err
+$COMPILER 1 input/a23_String.zz  > output/a23_String.out 2> output/a23_String.err
+$COMPILER 1 input/a24_General.zz > output/a24_General.out 2> output/a24_General.err
 
 # ---------------------------------------------------------------------
 # - End of Tests (A12 - S21) ------------------------------------------
