1dbb49d151d86808a8dabf02ee39048a8a795421
Tyson Moyes <moye0025@algonquinlive.com>
2021-07-12 23:20:49 -0400

    change some stuff in zz_A22 batch script for consistency

--- zz_A22.bat
+++ zz_A22.bat
@@ -2,7 +2,7 @@
 ::
 :: COMPILERS COURSE - SCRIPT ---------------------------------------------
 
-SET COMPILER=Compilers.exe
+SET COMPILER=zz_compiler.exe
 
 REM ---------------------------------------------------------------------
 REM - Begin of Tests (A22 - S21) ----------------------------------------
@@ -13,10 +13,10 @@ REM ---------------------------------------------------------------------
 ::
 REM - Basic Tests (A22 - S21) - - - - - - - - - - - - - - - - - - - - - -
 ren *.exe %COMPILER%
-%COMPILER% 1 input/a22_Empty.zz  	> output/a22_empty.out		2> output/a22_empty.err
+%COMPILER% 1 input/a21_Empty.zz  	> output/a21_empty.out		2> output/a21_empty.err
 %COMPILER% 1 input/a22_Numbers.zz	> output/a22_numbers.out	2> output/a22_numbers.err
-%COMPILER% 1 input/a22_String.zz  	> output/a22_string.out 	2> output/a22_string.err
-%COMPILER% 1 input/a22_General.zz  	> output/a22_general.out 	2> output/a22_general.err
+%COMPILER% 1 input/a23_String.zz  	> output/a23_string.out 	2> output/a23_string.err
+%COMPILER% 1 input/a24_General.zz  	> output/a24_general.out 	2> output/a24_general.err
 
 REM ---------------------------------------------------------------------
 REM - End of Tests (A22 - S21) ------------------------------------------
