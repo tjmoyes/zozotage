83134946d1b67d64e5728cd3652585fcbbf7e86a
Tyson Moyes <tysonjfmoyes@gmail.com>
2021-08-11 13:47:30 -0400

    Changed datatypes in Parser.c to zz datatypes, and commented out a couple switch cases that do not apply to us

--- code/Parser.c
+++ code/Parser.c
@@ -2,10 +2,11 @@
 * COMPILERS COURSE - Algonquin College
 * Code version: Summer, 2021
 * Author: Svillen Ranev - Paulo Sousa.
+* Contributors: Jon Liu 040967432, Tyson Moyes 040761903
 *************************************************************
 * File name: Parser.c
 * Compiler: MS Visual Studio 2019
-* Course: CST 8152 – Compilers, Lab Section: [011, 012, 013]
+* Course: CST 8152 – Compilers, Lab Section: 013
 * Assignment: A3.
 * Date: May 01 2021
 * Professor: Paulo Sousa
@@ -25,23 +26,25 @@
  * Process Parser
  ************************************************************/
 
-sofia_nul startParser() {
+zz_nul startParser()
+{
 	lookahead = tokenizer();
 	program();
 	matchToken(SEOF_T, NO_ATTR);
 	printf("%s%s\n", STR_LANGNAME, ": Source file parsed");
 }
 
-
 /*************************************************************
  * Match Token
  ************************************************************/
 /* TODO: This is the main code for match - check your definition */
-sofia_nul matchToken(sofia_int tokenCode, sofia_int tokenAttribute) {
-	sofia_int matchFlag = 1;
-	switch (lookahead.code) {
+zz_nul matchToken(zz_int tokenCode, zz_int tokenAttribute)
+{
+	zz_int matchFlag = 1;
+	switch (lookahead.code)
+	{
 	case KW_T:
-	// TO_DO: Include the other cases
+		// TO_DO: Include the other cases
 		if (lookahead.attribute.codeType != tokenAttribute)
 			matchFlag = 0;
 	default:
@@ -50,9 +53,11 @@ sofia_nul matchToken(sofia_int tokenCode, sofia_int tokenAttribute) {
 	}
 	if (matchFlag && lookahead.code == SEOF_T)
 		return;
-	if (matchFlag) {
+	if (matchFlag)
+	{
 		lookahead = tokenizer();
-		if (lookahead.code == ERR_T) {
+		if (lookahead.code == ERR_T)
+		{
 			printError();
 			lookahead = tokenizer();
 			syntaxErrorNumber++;
@@ -66,10 +71,12 @@ sofia_nul matchToken(sofia_int tokenCode, sofia_int tokenAttribute) {
  * Syncronize Error Handler
  ************************************************************/
 /* TODO: This is the function to handler error - adjust basically datatypes */
-sofia_nul syncErrorHandler(sofia_int syncTokenCode) {
+zz_nul syncErrorHandler(zz_int syncTokenCode)
+{
 	printError();
 	syntaxErrorNumber++;
-	while (lookahead.code != syncTokenCode) {
+	while (lookahead.code != syncTokenCode)
+	{
 		if (lookahead.code == SEOF_T)
 			exit(syntaxErrorNumber);
 		lookahead = tokenizer();
@@ -82,11 +89,13 @@ sofia_nul syncErrorHandler(sofia_int syncTokenCode) {
  * Print Error
  ************************************************************/
 /* TODO: This is the function to error printing - adjust basically datatypes */
-sofia_nul printError() {
+zz_nul printError()
+{
 	Token t = lookahead;
 	printf("%s%s%3d\n", STR_LANGNAME, ": Syntax error:  Line:", line);
 	printf("*****  Token code:%3d Attribute: ", t.code);
-	switch (t.code) {
+	switch (t.code)
+	{
 	case ERR_T:
 		printf("%s\n", t.attribute.errLexeme);
 		break;
@@ -99,14 +108,14 @@ sofia_nul printError() {
 	case KW_T:
 		printf("%s\n", keywordTable[t.attribute.codeType]);
 		break;
-	case ASS_OP_T:
+	//case ASS_OP_T:
 	case LPR_T:
 	case RPR_T:
 	case LBR_T:
 	case RBR_T:
-	case EOS_T:
-		printf("NA\n");
-		break;
+	//case EOS_T:
+	//	printf("NA\n");
+	//	break;
 	// TO_DO: Adjust the other cases
 	default:
 		printf("%s%s%d\n", STR_LANGNAME, ": Scanner error: invalid token code: ", t.code);
@@ -118,10 +127,13 @@ sofia_nul printError() {
  * BNF: <program> -> PLATYPUS { <opt_statements> }
  * FIRST(<program>)= {KW_T (MAIN)}.
  ************************************************************/
-sofia_nul program() {
-	switch (lookahead.code) {
+zz_nul program()
+{
+	switch (lookahead.code)
+	{
 	case KW_T:
-		if (lookahead.attribute.codeType == MAIN) {
+		if (lookahead.attribute.codeType == MAIN)
+		{
 			matchToken(KW_T, MAIN);
 			matchToken(LBR_T, NO_ATTR);
 			dataSession();
@@ -129,11 +141,11 @@ sofia_nul program() {
 			matchToken(RBR_T, NO_ATTR);
 			break;
 		}
-		else {
+		else
+		{
 			printError();
 		}
-	case SEOF_T:
-		; // Empty: for optional
+	case SEOF_T:; // Empty: for optional
 		break;
 	default:
 		printError();
@@ -141,13 +153,13 @@ sofia_nul program() {
 	printf("%s%s\n", STR_LANGNAME, ": Program parsed");
 }
 
-
 /*************************************************************
  * dataSession
  * BNF: <dataSession> -> DATA { <opt_varlist_declarations> }
  * FIRST(<program>)= {KW_T (DATA)}.
  ************************************************************/
-sofia_nul dataSession() {
+zz_nul dataSession()
+{
 	matchToken(KW_T, DATA);
 	matchToken(LBR_T, NO_ATTR);
 	optVarListDeclarations();
@@ -160,7 +172,8 @@ sofia_nul dataSession() {
  * BNF: <opt_varlist_declarations> -> <varlist_declarations> | e
  * FIRST(<opt_varlist_declarations>) = { e, KW_T (INT), KW_T (FLOAT), KW_T (STRING)}.
  ************************************************************/
-sofia_nul optVarListDeclarations() {
+zz_nul optVarListDeclarations()
+{
 	// TO_DO: Basic implementation
 	; // Empty
 	printf("%s%s\n", STR_LANGNAME, ": Optional Variable List Declarations parsed");
@@ -171,7 +184,8 @@ sofia_nul optVarListDeclarations() {
  * BNF: <codeSession> -> CODE { <opt_statements> }
  * FIRST(<codeSession>)= {KW_T (CODE)}.
  ************************************************************/
-sofia_nul codeSession() {
+zz_nul codeSession()
+{
 	matchToken(KW_T, CODE);
 	matchToken(LBR_T, NO_ATTR);
 	optionalStatements();
@@ -187,15 +201,17 @@ sofia_nul codeSession() {
  * FIRST(<opt_statements>) = { ϵ , IVID_T, FVID_T, SVID_T, KW_T(IF),
  *				KW_T(WHILE), KW_T(READ), KW_T(WRITE) }
  ************************************************************/
-sofia_nul optionalStatements() {	// TO_DO: Basic implementation
-	switch (lookahead.code) {
+zz_nul optionalStatements()
+{ // TO_DO: Basic implementation
+	switch (lookahead.code)
+	{
 	case KW_T:
-		if (lookahead.attribute.codeType == WRITE) {
+		if (lookahead.attribute.codeType == WRITE)
+		{
 			statements();
 			break;
 		}
-	default:
-		; // Empty
+	default:; // Empty
 	}
 	printf("%s%s\n", STR_LANGNAME, ": Optional statements parsed");
 }
@@ -206,7 +222,8 @@ sofia_nul optionalStatements() {	// TO_DO: Basic implementation
  * FIRST(<statements>) = { IVID_T, FVID_T, SVID_T, KW_T(IF),
  *		KW_T(WHILE), KW_T(READ), KW_T(WRITE) }
  ************************************************************/
-sofia_nul statements() {
+zz_nul statements()
+{
 	statement();
 	statementsPrime();
 	printf("%s%s\n", STR_LANGNAME, ": Statements parsed");
@@ -218,16 +235,18 @@ sofia_nul statements() {
  * FIRST(<statementsPrime>) = { ϵ , IVID_T, FVID_T, SVID_T, 
  *		KW_T(IF), KW_T(WHILE), KW_T(READ), KW_T(WRITE) }
  ************************************************************/
-sofia_nul statementsPrime() {
-	switch (lookahead.code) {
+zz_nul statementsPrime()
+{
+	switch (lookahead.code)
+	{
 	case KW_T:
-		if (lookahead.attribute.codeType == WRITE) {
+		if (lookahead.attribute.codeType == WRITE)
+		{
 			statement();
 			statementsPrime();
 			break;
 		}
-	default:
-		; //empty string
+	default:; //empty string
 	}
 }
 
@@ -238,10 +257,13 @@ sofia_nul statementsPrime() {
  * FIRST(<statement>) = { IVID_T, FVID_T, SVID_T, KW_T(IF), KW_T(WHILE),
  *			KW_T(READ), KW_T(WRITE) }
  ************************************************************/
-sofia_nul statement() {
-	switch (lookahead.code) {
+zz_nul statement()
+{
+	switch (lookahead.code)
+	{
 	case KW_T:
-		switch (lookahead.attribute.codeType) {
+		switch (lookahead.attribute.codeType)
+		{
 		case WRITE:
 			outputStatement();
 			break;
@@ -260,7 +282,8 @@ sofia_nul statement() {
  * BNF: <output statement> -> WRITE (<output statementPrime>);
  * FIRST(<output statement>) = { KW_T(WRITE) }
  ************************************************************/
-sofia_nul outputStatement() {
+zz_nul outputStatement()
+{
 	matchToken(KW_T, WRITE);
 	matchToken(LPR_T, NO_ATTR);
 	outputVariableList();
@@ -274,13 +297,14 @@ sofia_nul outputStatement() {
  * BNF: <opt_variable list> -> <variable list> | ϵ
  * FIRST(<opt_variable_list>) = { IVID_T, FVID_T, SVID_T, ϵ }
  ************************************************************/
-sofia_nul outputVariableList() {
-	switch (lookahead.code) {
+zz_nul outputVariableList()
+{
+	switch (lookahead.code)
+	{
 	case STR_T:
 		matchToken(STR_T, NO_ATTR);
 		break;
-	default:
-		;
+	default:;
 	}
 	printf("%s%s\n", STR_LANGNAME, ": Output variable list parsed");
 }
